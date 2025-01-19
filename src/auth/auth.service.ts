fe987aa7f3fa62ae6c4e9faf03b020ca33c93471
SantiagoMartinez <smartinez9626@gmail.com>
2025-01-19 11:54:18 -0500

    refactor authentication module; remove unused DTOs and implement user registration with password hashing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,45 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: await bcrypt.hash(password, 10),
+      });
+      await this.userRepository.save(user);
+      // Manejar el retorno de la información sensible
+      delete user.password;
+      return user;
+      // TODO: Implementar JWT
+    } catch (error) {
+      this.handleDBErrors(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+  private handleDBErrors(error: any): never {
+    if (error.code === '23505') throw new BadRequestException(error.detail);
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+    console.log(error);
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    throw new InternalServerErrorException('Something went wrong');
   }
 }
